6a44ce4ecfd98470acbdfd13cc94bb1978e9d0cb
crimsoncomedian <mr.reganmath@gmail.com>
2025-09-11 06:27:59 -0400

    Update script_cf5a3173.js

--- assets/script_cf5a3173.js
+++ assets/script_cf5a3173.js
@@ -1,1 +1,12 @@
-
+function sendToGoogleSheet(data) {
+  fetch("https://script.google.com/macros/s/AKfycbyh0LI4oKDCtkQIQsaa80vbbqIdOykPi6S8tkqfruYld5Dy3-9riVKFqKLgwhoA-s6k/exec", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then(res => res.text())
+    .then(response => console.log("Submitted:", response))
+    .catch(err => console.error("Error submitting to sheet:", err));
+}
